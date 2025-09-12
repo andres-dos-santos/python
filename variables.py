3910de7cc1b4bb125207638e9182055f38afd2b6
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-09-12 09:46:45 -0300

    variables

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Ex.: 1
+
+name = "Andres"
+age = 24
+city = "Três Rios, RJ"
+
+print("Meu nome é", name, "tenho ", age, "anos e moro em ", city)
+
+# Ex.: 2
+
+x = 2
+y = 1.5
+
+print("A soma é: ", x + y)
+print("A subtração é: ", x - y)
+print("A multiplicação é: ", x * y)
+print("A divisão é: ", x / y)
+
+# Ex.: 3
+
+number = int(input("Digite um número: "))
+print("O dobro do número que você digitou é: ", number * 2)
+
+# Ex.: 4
+
+age = int(input("Digite a sua idade: "))
+print("Você é ", "maior de idade" if age > 18 else "menor de idade.")
