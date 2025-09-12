57a1e51a4c1eb7857c26e43a9b147d28eae7b866
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-09-12 15:55:27 -0300

    sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+fruits = {"maça", "banana", "laranja"}
+numbers = set([1, 2, 3, 4])
+
+fruits.add("uva")
+
+print(fruits)
+
+list = [1, 2, 2, 3, 3, 4]
+no_duplicates = set(list)
+print(no_duplicates)
